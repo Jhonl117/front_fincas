ff0417c0299038b8f25e5f7918e053f4b4df0dd0
Jader-Montoya <jadermontoya283@gmail.com>
2023-09-05 22:02:22 -0500

    Cambios y modificaciones en el modulo clientes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,7 +6,15 @@ const router = Router()
 
 
 // ================================= \\
-// Ruta index
+// Rutas index
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
 
 router.get('/', (req, res) => {
     res.render('index')
@@ -14,10 +22,10 @@ router.get('/', (req, res) => {
 
 
 // ================================= \\
-// Ruta usuario
+// Rutas Usuarios
 
-router.get('/usuarios', (req, res) => {
-    res.render('usuarios/usuarios')
+router.get('/listarUsuarios', (req, res) => {
+    res.render('usuarios/listarUsuarios')
 })
 
 router.get('/crearUsuario', (req, res) => {
@@ -26,11 +34,24 @@ router.get('/crearUsuario', (req, res) => {
 
 
 // ================================= \\
-// Ruta Empleado
+// Rutas Clientes
+
+router.get('/listarClientes', (req, res) => {
+    res.render('clientes/listarClientes')
+})
+
+router.get('/crearCliente', (req, res) => {
+    res.render('clientes/formClientes')
+})
+
+
+// ================================= \\
+// Rutas Empleado
 
 router.get('/empleados', (req, res) => {
     res.render('empleados')
 })
+
 
 // ================================= \\
 // Ruta Error
